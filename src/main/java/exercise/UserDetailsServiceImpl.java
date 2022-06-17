be905e5016a558814b340fcefaa4daebbeafe793
VYATCHESLAV BALAKHONOV <slaviklal@yandex.ru>
2022-06-17 03:43:06 +0300

    update a little

--- src/main/java/exercise/UserDetailsServiceImpl.java
+++ src/main/java/exercise/UserDetailsServiceImpl.java
@@ -4,10 +4,13 @@ import exercise.model.User;
 import exercise.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,10 +22,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         // BEGIN
         User user = repository.findByEmail(email);
+
+        List<SimpleGrantedAuthority> authorities = List.of(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+        );
+
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                user.getAuthorities());
+                authorities
+        );
         // END
     }
 }
